e44218b8c3d6aca53df111eef6a27202891c974c
Amit Singh <amitsingh100014@gmail.com>
2026-03-16 23:03:47 +0530

    add function

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  export default function Card7() {
   return (
-    <>
-   <h1 className='bg-green-400'>tailwind test</h1>
-     
-    </>
+    <div className="w-60 h-60 rounded-xl">
+      <img
+        src="https://media1.giphy.com/media/z8n8dWgQ0mgEIyzlmV/giphy.gif"
+        alt="Giphy"
+        className="rounded-t-xl"
+      />
+      <div className="glass py-4 px-5 relative -top-[3.4rem] rounded-b-xl z-10">
+        <h1 className="font-bold font-mono text-xl">
+          Available soon
+        </h1>
+      </div>
+    </div>
   )
+}
+
+
 }
 
 export default App
